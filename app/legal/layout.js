97fa6dc436b70b5282fc0b8682a5392e2e6cb2e4
Wisdom Ojimah <vindicatedwisdom@gmail.com>
2023-02-08 19:49:39 +0100

    chore: changed faqs page content

--- app/legal/layout.js
+++ app/legal/layout.js
@@ -18,7 +18,7 @@ const Layout = ({ children }) => {
 
     return (
 		<>
-			<div className="grid gap-8 items-start lg:grid-cols-12 p-[5%] xl:p-[7%] lg:gap-12">
+			<main className="grid gap-8 items-start lg:grid-cols-12 p-[5%] xl:p-[7%] lg:gap-12">
 				<div
 					className={`fixed z-[1024] transiton-transform ease-in-out left-0 px-[5%] top-[10%] duration-500 lg:sticky lg:z-auto lg:w-auto lg:p-0 w-full lg:top-[15%] lg:col-span-3 bg-white py-4 ${
 						isActive
@@ -66,7 +66,7 @@ const Layout = ({ children }) => {
 				</div>
 
 				<div className="lg:col-span-9">{children}</div>
-			</div>
+			</main>
 
 			<button
 				className="fixed right-[5%] bottom-[15%] rounded-md px-2.5 pt-1 pb-0.5 bg-gray-100 z-[1024] lg:hidden"
