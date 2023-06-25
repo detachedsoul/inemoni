0ce944f64c02fc666b6ffa047c79feee402a9e3f
Wisdom Ojimah <vindicatedwisdom@gmail.com>
2023-06-25 14:20:10 +0100

    fix(design): fixed text overflowing container in transaction table

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -95,7 +95,7 @@ const Dashboard = () => {
                         </h2>
 
                         <div className="rounded-[20px] bg-[#F2F2F2] px-4 pb-4 overflow-x-auto">
-                            <table className="w-full whitespace-nowrap table-auto border-collapse">
+                            <table className="w-full border-collapse table-auto whitespace-nowrap h-full">
                                 <thead className="border-b border-[#979797] text-left">
                                     <tr className="text-[#666666]">
                                         <th className="font-normal pt-4 pb-2">
@@ -118,25 +118,25 @@ const Dashboard = () => {
 
                                 <tbody>
                                     <tr className="border-b border-[#979797] last:border-none">
-                                        <td className="py-3 flex items-center gap-2.5">
-                                            <Image className="h-12 w-12" src={BankTransfer} alt="Bank Transfer" />
+                                        <td className="pr-4 py-2 flex items-center gap-2.5 min-w-max h-full">
+                                            <Image className="h-11 w-11" src={BankTransfer} alt="Bank Transfer" />
 
-                                            <div className="-space-y-2">
-                                                <h4 className="font-medium leading-none">
+                                            <div>
+                                                <p className="font-medium text-sm -mb-1.5">
                                                     Joseph Chikadibia
-                                                </h4>
+                                                </p>
 
-                                                <p className="bg-successful-bg text-successful rounded-full py-1 px-3 text-center font-medium uppercase text-[0.6rem] inline-block leading-none">
+                                                <p className="bg-successful-bg text-successful rounded-full py-1 px-3 text-center font-medium uppercase text-[0.5rem] inline-block leading-none">
                                                     Successful
                                                 </p>
                                             </div>
                                         </td>
 
-                                        <td className="py-3 font-medium">
+                                        <td className="pr-4 py-2 font-medium">
                                             Bank Transfer
                                         </td>
 
-                                        <td className="py-3 -space-y-1">
+                                        <td className="pr-4 py-2 -space-y-1.5">
                                             <p className="font-medium">
                                                 15 June, 2023
                                             </p>
@@ -146,31 +146,31 @@ const Dashboard = () => {
                                             </p>
                                         </td>
 
-                                        <td className="py-3 text-successful font-medium">
+                                        <td className="pr-4 py-2 text-successful font-medium">
                                             ₦5,780.00
                                         </td>
                                     </tr>
 
                                     <tr className="border-b border-[#979797] last:border-none">
-                                        <td className="py-3 flex items-center gap-2.5">
-                                            <Image className="h-12 w-12" src={DollarCard} alt="Dollar Card" />
+                                        <td className="pr-4 py-2 flex items-center gap-2.5 min-w-max h-full m-0">
+                                            <Image className="h-11 w-11" src={DollarCard} alt="Dollar Card" />
 
-                                            <div className="-space-y-2">
-                                                <h4 className="font-medium leading-none">
+                                            <div className="">
+                                                <p className="font-medium text-sm -mb-1.5">
                                                     Dollar Virtual Card
-                                                </h4>
+                                                </p>
 
-                                                <p className="bg-pending-bg text-pending rounded-full py-1 px-3 text-center font-medium uppercase text-[0.6rem] inline-block leading-none">
+                                                <p className="bg-pending-bg text-pending rounded-full py-1 px-3 text-center font-medium uppercase text-[0.5rem] inline-block leading-none">
                                                     Pending
                                                 </p>
                                             </div>
                                         </td>
 
-                                        <td className="py-3 font-medium">
+                                        <td className="pr-4 py-2 font-medium">
                                             Virtual Card Creation
                                         </td>
 
-                                        <td className="py-3 -space-y-1">
+                                        <td className="pr-4 py-2 -space-y-1.5">
                                             <p className="font-medium">
                                                 15 June, 2023
                                             </p>
@@ -180,31 +180,31 @@ const Dashboard = () => {
                                             </p>
                                         </td>
 
-                                        <td className="py-3 text-pending font-medium">
+                                        <td className="pr-4 py-2 text-pending font-medium">
                                             ₦5,780.00
                                         </td>
                                     </tr>
 
                                     <tr className="border-b border-[#979797] last:border-none">
-                                        <td className="py-3 flex items-center gap-2.5">
-                                            <Image className="h-12 w-12" src={Wallet} alt="Inemoni Wallet" />
+                                        <td className="pr-4 py-2 flex items-center gap-2.5 min-w-max h-full m-0">
+                                            <Image className="h-11 w-11" src={Wallet} alt="Inemoni Wallet" />
 
-                                            <div className="-space-y-2">
-                                                <h4 className="font-medium leading-none">
+                                            <div className="">
+                                                <p className="font-medium text-sm -mb-1.5">
                                                     Top-Up
-                                                </h4>
+                                                </p>
 
-                                                <p className="bg-failed-bg text-failed rounded-full py-1 px-3 text-center font-medium uppercase text-[0.6rem] inline-block leading-none">
+                                                <p className="bg-failed-bg text-failed rounded-full py-1 px-3 text-center font-medium uppercase text-[0.5rem] inline-block leading-none">
                                                     Failed
                                                 </p>
                                             </div>
                                         </td>
 
-                                        <td className="py-3 font-medium">
+                                        <td className="pr-4 py-2 font-medium">
                                             Wallet Top-Up
                                         </td>
 
-                                        <td className="py-3 -space-y-1">
+                                        <td className="pr-4 py-2 -space-y-1.5">
                                             <p className="font-medium">
                                                 15 June, 2023
                                             </p>
@@ -214,31 +214,31 @@ const Dashboard = () => {
                                             </p>
                                         </td>
 
-                                        <td className="py-3 text-failed font-medium">
+                                        <td className="pr-4 py-2 text-failed font-medium">
                                             ₦5,780.00
                                         </td>
                                     </tr>
 
                                     <tr className="border-b border-[#979797] last:border-none">
-                                        <td className="py-3 flex items-center gap-2.5">
-                                            <Image className="h-12 w-12" src={BankTransfer} alt="Bank Transfer" />
+                                        <td className="pr-4 py-2 flex items-center gap-2.5 min-w-max h-full m-0">
+                                            <Image className="h-11 w-11" src={BankTransfer} alt="Bank Transfer" />
 
-                                            <div className="-space-y-2">
-                                                <h4 className="font-medium leading-none">
+                                            <div className="">
+                                                <p className="font-medium text-sm -mb-1.5">
                                                     Joseph Chikadibia
-                                                </h4>
+                                                </p>
 
-                                                <p className="bg-successful-bg text-successful rounded-full py-1 px-3 text-center font-medium uppercase text-[0.6rem] inline-block leading-none">
+                                                <p className="bg-successful-bg text-successful rounded-full py-1 px-3 text-center font-medium uppercase text-[0.5rem] inline-block leading-none">
                                                     Successful
                                                 </p>
                                             </div>
                                         </td>
 
-                                        <td className="py-3 font-medium">
+                                        <td className="pr-4 py-2 font-medium">
                                             Bank Transfer
                                         </td>
 
-                                        <td className="py-3 -space-y-1">
+                                        <td className="pr-4 py-2 -space-y-1.5">
                                             <p className="font-medium">
                                                 15 June, 2023
                                             </p>
@@ -248,31 +248,31 @@ const Dashboard = () => {
                                             </p>
                                         </td>
 
-                                        <td className="py-3 text-successful font-medium">
+                                        <td className="pr-4 py-2 text-successful font-medium">
                                             ₦5,780.00
                                         </td>
                                     </tr>
 
                                     <tr className="border-b border-[#979797] last:border-none">
-                                        <td className="py-3 flex items-center gap-2.5">
-                                            <Image className="h-12 w-12" src={DollarCard} alt="Dollar Card" />
+                                        <td className="pr-4 py-2 flex items-center gap-2.5 min-w-max h-full m-0">
+                                            <Image className="h-11 w-11" src={DollarCard} alt="Dollar Card" />
 
-                                            <div className="-space-y-2">
-                                                <h4 className="font-medium leading-none">
+                                            <div className="">
+                                                <p className="font-medium text-sm -mb-1.5">
                                                     Dollar Virtual Card
-                                                </h4>
+                                                </p>
 
-                                                <p className="bg-pending-bg text-pending rounded-full py-1 px-3 text-center font-medium uppercase text-[0.6rem] inline-block leading-none">
+                                                <p className="bg-pending-bg text-pending rounded-full py-1 px-3 text-center font-medium uppercase text-[0.5rem] inline-block leading-none">
                                                     Pending
                                                 </p>
                                             </div>
                                         </td>
 
-                                        <td className="py-3 font-medium">
+                                        <td className="pr-4 py-2 font-medium">
                                             Virtual Card Creation
                                         </td>
 
-                                        <td className="py-3 -space-y-1">
+                                        <td className="pr-4 py-2 -space-y-1.5">
                                             <p className="font-medium">
                                                 15 June, 2023
                                             </p>
@@ -282,31 +282,31 @@ const Dashboard = () => {
                                             </p>
                                         </td>
 
-                                        <td className="py-3 text-pending font-medium">
+                                        <td className="pr-4 py-2 text-pending font-medium">
                                             ₦5,780.00
                                         </td>
                                     </tr>
 
                                     <tr className="border-b border-[#979797] last:border-none">
-                                        <td className="py-3 flex items-center gap-2.5">
-                                            <Image className="h-12 w-12" src={Wallet} alt="Inemoni Wallet" />
+                                        <td className="pr-4 py-2 flex items-center gap-2.5 min-w-max h-full m-0">
+                                            <Image className="h-11 w-11" src={Wallet} alt="Inemoni Wallet" />
 
-                                            <div className="-space-y-2">
-                                                <h4 className="font-medium leading-none">
+                                            <div className="">
+                                                <p className="font-medium text-sm -mb-1.5">
                                                     Top-Up
-                                                </h4>
+                                                </p>
 
-                                                <p className="bg-failed-bg text-failed rounded-full py-1 px-3 text-center font-medium uppercase text-[0.6rem] inline-block leading-none">
+                                                <p className="bg-failed-bg text-failed rounded-full py-1 px-3 text-center font-medium uppercase text-[0.5rem] inline-block leading-none">
                                                     Failed
                                                 </p>
                                             </div>
                                         </td>
 
-                                        <td className="py-3 font-medium">
+                                        <td className="pr-4 py-2 font-medium">
                                             Wallet Top-Up
                                         </td>
 
-                                        <td className="py-3 -space-y-1">
+                                        <td className="pr-4 py-2 -space-y-1.5">
                                             <p className="font-medium">
                                                 15 June, 2023
                                             </p>
@@ -316,7 +316,7 @@ const Dashboard = () => {
                                             </p>
                                         </td>
 
-                                        <td className="py-3 text-failed font-medium">
+                                        <td className="pr-4 py-2 text-failed font-medium">
                                             ₦5,780.00
                                         </td>
                                     </tr>
